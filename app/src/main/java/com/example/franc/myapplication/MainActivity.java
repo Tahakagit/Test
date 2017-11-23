504318151ef4b8af518498c4369f843b4b957463
Tahakagit <francescodar@gmail.com>
2017-11-23 13:49:00 +0100

    versione stabile

--- app/src/main/java/com/example/franc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/franc/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.franc.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
-
+    Realm mRealm = Realm.getDefaultInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        RecyclerViewFragment rvfragment = new RecyclerViewFragment();
+        LayoutInflater inflater = this.getLayoutInflater();
+/*
+        final View yourCustomView = inflater.inflate(R.layout., null);
+*/
+
+        RecyclerViewFragment.SimpleRVAdapter adapter = new RecyclerViewFragment.SimpleRVAdapter("prova");
+        super.onCreate(savedInstanceState);
+        RecyclerView rview = findViewById(R.id.recyclerview);
+        rview.setAdapter(adapter);
+
+   }
 }
